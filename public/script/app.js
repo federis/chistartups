cdcb00e5338a1a65dda169af59e2796bad0b03e7
Asaf Elani <aelani@gmail.com>
2014-10-16 13:03:24 -0500

    Add email to flyover to notify of form submission

--- public/script/app.js
+++ public/script/app.js
@@ -73,7 +73,49 @@ $(function() {
     $('#add-account-form').removeClass('visible')
   })
 
+  $('#contact input').on('focus', function() {
+    $('#alert')
+      .removeClass('success')
+      .removeClass('fail')
+  })
+
+  function isValidEmail(email) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i)
+    return pattern.test(email)
+  }
+
+  var timeout
+
   $('#contact').submit(function() {
+
+    clearTimeout(timeout)
+
+    $('#alert')
+      .removeClass('success')
+      .removeClass('fail')
+
+    if ($('#contact input:blank').length > 0) {
+      $('#alert')
+        .text('All fields are required.')
+        .removeClass('success')
+        .addClass('fail')
+      timeout = setTimeout(function() {
+        $('#alert').removeClass('fail')
+      }, 10000)
+      return false
+    }
+
+    if (!isValidEmail($('#emailField').val())) {
+      $('#alert')
+        .text('You must enter a valid email address.')
+        .removeClass('success')
+        .addClass('fail')
+      timeout = setTimeout(function() {
+        $('#alert').removeClass('fail')
+      }, 10000)
+      return false    
+    }
+
     ga('send', 'event', 'form', 'submit')
     $.ajax({
       type: "POST",
@@ -88,7 +130,7 @@ $(function() {
             .text('Thanks! We\'ll update the map shortly.')
             .removeClass('fail')
             .addClass('success')
-          setTimeout(function() {
+          timeout = setTimeout(function() {
             $('#alert').removeClass('success')
           }, 10000)
         } else {
@@ -97,12 +139,13 @@ $(function() {
             .text('Unable to submit form. Please check the information and re-submit.')
             .removeClass('success')
             .addClass('fail')
-          setTimeout(function() {
+          timeout = setTimeout(function() {
             $('#alert').removeClass('fail')
           }, 10000)
         }
       }
     })
+
     return false
   })
 })
--- twitter-stream.js
+++ twitter-stream.js
@@ -28,26 +28,32 @@ var watchStartups = [
   // begin testing accounts
   /*
   {
+    // @espn
     twitter_id: '2557521',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @TMZ
     twitter_id: '16331010',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @redeyechicago
     twitter_id: '16381558',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @nytimes
     twitter_id: '807095',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @FoxNews
     twitter_id: '1367531',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @cnnbrk
     twitter_id: '428333',
     lat: 41.8883695,
     lon: -87.6353645
@@ -55,343 +61,423 @@ var watchStartups = [
   */
   // end testing accounts
   {
+    // @FlyoverWorks
     twitter_id: '2600293579',
     lat: 41.89624,
     lon: -87.632462
   }, {
+    // @ycharts
     twitter_id: '22869238',
     lat: 41.8896996,
     lon: -87.6371928
   }, {
+    // @rocketmiles
     twitter_id: '1109586098',
     lat: 41.883434,
     lon: -87.6420629
   }, {
+    // @foodgenius
     twitter_id: '134637509',
     lat: 41.89374,
     lon: -87.63533
   }, {
+    // @ScoutAlarm
     twitter_id: '1019117701',
     lat: 41.886017,
     lon: -87.656462
   }, {
+    // @lightbank
     twitter_id: '115186995',
     lat: 41.8967156,
     lon: -87.6432804
   }, {
+    // @CatervaInc
     twitter_id: '205870141',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @retrofitme
     twitter_id: '336883384',
     lat: 41.8841061,
     lon: -87.6365326
   }, {
+    // @styleseek
     twitter_id: '540104759',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @GiveForward
     twitter_id: '15984241',
     lat: 41.9099067,
     lon: -87.6774502
   }, {
+    // @ownkiwi
     twitter_id: '37490931',
     lat: 41.9594952,
     lon: -87.6745535
   }, {
+    // @eSparkLearning
     twitter_id: '117252513',
     lat: 41.8783109,
     lon: -87.648055
   }, {
+    // @TrunkClub
     twitter_id: '19734728',
     lat: 41.892116,
     lon: -87.636659
   }, {
+    // @OpenAirplane
     twitter_id: '353354346',
     lat: 41.871902,
     lon: -87.633247
   }, {
+    // @1871Chicago
     twitter_id: '427424201',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @MentorMob
     twitter_id: '195633064',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @MobileXLabs
     twitter_id: '341130570',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @cloudbot
     twitter_id: '219573413',
     lat: 41.9107785,
     lon: -87.6781239
   }, {
+    // @Tempo_IQ
     twitter_id: '417595294',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @narrativesci
     twitter_id: '139819194',
     lat: 41.8868264,
     lon: -87.6182722
   }, {
+    // @SandboxInd
     twitter_id: '59518194',
     lat: 41.886017,
     lon: -87.656462
   }, {
+    // @BellyCard
     twitter_id: '328723269',
     lat: 41.8967156,
     lon: -87.6432804
   }, {
+    // @groovebug
     twitter_id: '281769966',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @Pear_says
     twitter_id: '546356652',
     lat: 41.8976465,
     lon: -87.6363531
   }, {
+    // @picturelife
     twitter_id: '296980645',
     lat: 41.877814,
     lon: -87.641833
   }, {
+    // @hydeparkangels
     twitter_id: '364945587',
     lat: 41.7893998,
     lon: -87.5963929
   }, {
+    // @BucketFeet
     twitter_id: '93684143',
     lat: 41.884035,
     lon: -87.651168
   }, {
+    // @CatapultChi
     twitter_id: '407915123',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @Learnmetrics
     twitter_id: '1011692930',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @RaiseMarket
     twitter_id: '1176649850',
     lat: 41.8953172,
     lon: -87.6365723
   }, {
+    // @SproutSocial
     twitter_id: '42793960',
     lat: 41.8826584,
     lon: -87.656986
   }, {
+    // @SpotHero
     twitter_id: '36217679',
     lat: 41.8958753,
     lon: -87.6349167
   }, {
+    // @threadless
     twitter_id: '5380672',
     lat: 41.8822481,
     lon: -87.659058
   }, {
+    // @GetCentUp
     twitter_id: '627482902',
     lat: 41.8875879,
     lon: -87.6779522
   }, {
+    // @StageBloc
     twitter_id: '15858514',
     lat: 41.9279859,
     lon: -87.7043763
   }, {
+    // @ParkWhiz
     twitter_id: '16870480',
     lat: 41.8788596,
     lon: -87.6428843
   }, {
+    // @gofooda
     twitter_id: '217190520',
     lat: 41.8967156,
     lon: -87.6432804
   }, {
+    // @ChicagoVentures
     twitter_id: '170011830',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @FanFueled
     twitter_id: '59813251',
     lat: 41.8895507,
     lon: -87.6281939
   }, {
+    // @Technori
     twitter_id: '176211652',
     lat: 41.8902773,
     lon: -87.6322469
   }, {
+    // @benchprep
     twitter_id: '33389501',
     lat: 41.8967156,
     lon: -87.6432804
   }, {
+    // @stathat
     twitter_id: '211223932',
     lat: 41.976667,
     lon: -87.674141
   }, {
+    // @brideside
     twitter_id: '553670217',
     lat: 41.8923914,
     lon: -87.6364606
   }, {
+    // @netgamix
     twitter_id: '207842824',
     lat: 41.8789887,
     lon: -87.6371429
   }, {
+    // @Optyn
     twitter_id: '410360307',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @GoProcured
     twitter_id: '436640690',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @MusicDealers
     twitter_id: '23459000',
     lat: 41.8771324,
     lon: -87.6427555
   }, {
+    // @Doggyloot
     twitter_id: '226727658',
     lat: 41.886017,
     lon: -87.656462
   }, {
+    // @hydeparkvp
     twitter_id: '576269965',
     lat: 41.8830087,
     lon: -87.6351499
   }, {
+    // @dabblehq
     twitter_id: '274179758',
     lat: 41.917103,
     lon: -87.68311
   }, {
+    // @ohkumbuya
     twitter_id: '291321080',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @SimpleRelevance
     twitter_id: '392753093',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @everpurse
     twitter_id: '604113352',
     lat: 41.8958753,
     lon: -87.6349167
   }, {
+    // @SceneTap
     twitter_id: '292717162',
     lat: 41.8870387,
     lon: -87.6270653
   }, {
+    // @AvantCredit
     twitter_id: '1392277274',
     lat: 41.9129859,
     lon: -87.633154
   }, {
+    // @publicgood
     twitter_id: '1408489836',
     lat: 41.8762169,
     lon: -87.6282726
   }, {
+    // @getbase
     twitter_id: '35222831',
     lat: 41.8958753,
     lon: -87.6349167
   }, {
+    // @GeoramaTravel
     twitter_id: '231949620',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @SwiftIQ
     twitter_id: '1706394698',
     lat: 41.882401,
     lon: -87.637816
   }, {
+    // @Resultly
     twitter_id: '305769956',
     lat: 41.8902504,
     lon: -87.6319071
   }, {
+    // @WeDelivr
     twitter_id: '906365376',
     lat: 41.8896869,
     lon: -87.6323132
   }, {
+    // @caremerge
     twitter_id: '538681567',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @Winestyr
     twitter_id: '264289725',
     lat: 41.8902166,
     lon: -87.6453862
   }, {
+    // @Aisle50
     twitter_id: '330424809',
     lat: 41.8847792,
     lon: -87.6447697
   }, {
+    // @starterleague
     twitter_id: '286674780',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @ExpertBids
     twitter_id: '176977952',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @CareTreeMe
     twitter_id: '789555043',
     lat: 41.901796,
     lon: -87.689621
   }, {
+    // @smallbizhive
     twitter_id: '415814667',
     lat: 41.895865,
     lon: -87.6354269
   }, {
+    // @BetterWeekdays
     twitter_id: '394882322',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @MarkITx
     twitter_id: '370177045',
     lat: 41.8870387,
     lon: -87.6270653
   }, {
+    // @ReviewTrackers
     twitter_id: '558149323',
     lat: 41.90913,
     lon: -87.674983
   }, {
+    // @rivscom
     twitter_id: '310379143',
     lat: 41.8799651,
     lon: -87.6508307
   }, {
+    // @bigmarker
     twitter_id: '139324845',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @Leasabl
     twitter_id: '204159806',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @LearnCore
     twitter_id: '240742457',
     lat: 41.8880653,
     lon: -87.6307608
   }, {
+    // @signalengage
     twitter_id: '82959314',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @OurLabel
     twitter_id: '190911858',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @Spotlite_com
     twitter_id: '497350670',
     lat: 41.892617,
     lon: -87.634916
   }, {
+    // @BlazePortfolio
     twitter_id: '218351766',
     lat: 41.963728,
     lon: -87.673581
   }, {
+    // @TableSAVVY
     twitter_id: '255834014',
     lat: 41.8883695,
     lon: -87.6353645
   }, {
+    // @upcityinc
     twitter_id: '952497528',
     lat: 41.893377,
     lon: -87.638631
   }, {
+    // @SpartzInc
     twitter_id: '226694810',
     lat: 41.8931808,
     lon: -87.6382342
   }, {
+    // @KLUTCHclub
     twitter_id: '411895650',
     lat: 41.906116,
     lon: -87.64882
   }, {
+    // @inklinghq
     twitter_id: '19910585',
-    lat: 41.9031361,
-    lon: -87.6286364
-  }, {
-    twitter_id: '856729849',
     lat: 41.9031361,
     lon: -87.6286364
   }
@@ -427,29 +513,45 @@ app.post('/contact', function(req, res) {
   , twitter = req.body.twitter
   , address = req.body.address
 
-  console.log(company + 
-    ', ' + twitter +
-    ', ' + address +
-    ', ' + email)
+  console.dir(req.body)
 
-  var emailMsg     = new sendgrid.Email({
+  var emailMsg = new sendgrid.Email({
     to:       email,
     from:     'asaf@flyoverworks.com',
     fromname: 'FlyoverWorks',
-    subject:  'Your Request to Add ' + company + ' to the CHITweets Map',
+    subject:  'Your Request to Add ' + company + ' to the CHI Tweets Map',
     html:     'Hi ' + name + ',<br/><br/>Thank you for submitting a request ' +
-              'to add ' + company + ' to the CHITweets Map. We\'re reviewing ' +
+              'to add ' + company + ' to the CHI Tweets Map. We\'re reviewing ' +
               'and verifying the information you provided and will update ' +
               'the map in the next 24 hours.<br/><br/>Asaf Elani<br/>' +
               '<a href="http://www.flyoverworks.com">FlyoverWorks</a>'
   })
+  
+  var formData = new sendgrid.Email({
+    to:       'asaf@flyoverworks.com',
+    from:     'chitweets@flyoverworks.com',
+    fromname: 'CHI Tweets',
+    subject:  'Request to Add ' + company + ' to CHI Tweets',
+    html:     '<p>Name: ' + name + '</p>' +
+              '<p>Email: ' + email + '</p>' +
+              '<p>Company: ' + company + '</p>' +
+              '<p>Twitter: ' + twitter + '</p>' +
+              '<p>Address: ' + address + '</p>'
+  })
 
-  sendgrid.send(emailMsg, function(err, json) {
-    if (err) { 
+  sendgrid.send(formData, function(err, json) {
+    if (err) {
       res.json(400)
       return console.error(err)
     } else {
-      res.json(200)
+      sendgrid.send(emailMsg, function(err, json) {
+        if (err) { 
+          res.json(400)
+          return console.error(err)
+        } else {
+          res.json(200)
+        }
+      })
     }
   })
 })
